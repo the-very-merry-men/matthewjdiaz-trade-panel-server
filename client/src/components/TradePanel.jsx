e55b56d7ce3b52cf9e3485a3622dcba259087ca6
umairnadeem123 <nadeem.umair123@gmail.com>
2019-05-22 18:18:34 -0700

    Implement a series of tests for React component using Enzyme

--- client/src/components/TradePanel.jsx
+++ client/src/components/TradePanel.jsx
@@ -49,7 +49,7 @@ class TradePanel extends Component {
             <h3>Buy {this.state.stock.toUpperCase()}</h3>
           </div>
           <div id='header-right' className={this.state.showOrderTypes ? 'active-bottom' : ''}>
-            <svg width='28' height='28' onClick={this.showOrderTypes.bind(this)}>
+            <svg id='menu' className={this.state.showOrderTypes ? 'active' : ''} width='28' height='28' onClick={this.showOrderTypes.bind(this)}>
               <path fillRule="evenodd" d="M14,16 C12.8954305,16 12,15.1045695 12,14 C12,12.8954305 12.8954305,12 14,12 C15.1045695,12 16,12.8954305 16,14 C16,15.1045695 15.1045695,16 14,16 Z M6,16 C4.8954305,16 4,15.1045695 4,14 C4,12.8954305 4.8954305,12 6,12 C7.1045695,12 8,12.8954305 8,14 C8,15.1045695 7.1045695,16 6,16 Z M22,16 C20.8954305,16 20,15.1045695 20,14 C20,12.8954305 20.8954305,12 22,12 C23.1045695,12 24,12.8954305 24,14 C24,15.1045695 23.1045695,16 22,16 Z"></path>
             </svg>
           </div>
--- test/TradePanel.test.jsx
+++ test/TradePanel.test.jsx
@@ -4,7 +4,45 @@ import TradePanel from '../client/src/components/TradePanel.jsx';
 import { shallow, render, mount } from 'enzyme';
 
 describe('Trade panel behavior', () => {
-  it('should show results for the current stock', () => {
+  it('renders without exploding', () => {
     const wrapper = shallow(<TradePanel/>);
-  })
-})
+    expect(wrapper.length).toEqual(1);
+  });
+
+  it('should show the ticker for the current stock', () => {
+    const wrapper = shallow(<TradePanel/>);
+    let stock = 'GOOG';
+    wrapper.setState({ stock }, () => {
+      const text = wrapper.find('h3').text();
+      expect(text).toBe(`Buy ${stock.toUpperCase()}`);
+    });
+  });
+
+  it('should toggle menu upon clicking the menu button', () => {
+    const wrapper = shallow(<TradePanel/>);
+    const menuBtn = wrapper.find('#menu');
+
+    // Toggle menu on
+    menuBtn.simulate('click');
+    expect(wrapper.state().showOrderTypes).toBe(true);
+    expect(wrapper.find('OrderTypes').exists()).toBe(true);
+
+    // Toggle menu off
+    menuBtn.simulate('click');
+    expect(wrapper.state().showOrderTypes).toBe(false);
+    expect(wrapper.find('OrderTypes').exists()).toBe(false);
+  });
+
+  it('should show market order option by default on load', () => {
+    const wrapper = shallow(<TradePanel/>);
+    let currType = wrapper.state().currType;
+    expect(currType).toEqual(0);
+    expect(wrapper.state().orderTypes[currType]).toBe('Market Order');
+  });
+
+  it.todo('should update the price upon changing quantity of shares');
+  it.todo('should render limit price input after changing order type to Limit Order');
+  it.todo('should render stop price input after changing order type to Stop Loss Order');
+  it.todo('should render stop price AND limit price inputs after changing order type to Stop Limit Order');
+  it.todo('should update commissions field dynamically upon changing share quantity');
+});
